526911aa50ada45063a945240e411d49719872a7
Carlos Iván <carlosivanhb@hotmail.com>
2024-11-11 08:56:44 -0600

    Clase corregida para manejo de paquetes con diferente tamaño

--- include/LoRaCommunicator.hpp
+++ include/LoRaCommunicator.hpp
@@ -3,92 +3,52 @@
 #define LORA_COMMUNICATOR_HEADER
 
 #include <heltec_unofficial.h>
-#include <array>
+#include <vector>
+#include <stdexcept>
 
 struct LoRaParameters {
-    float frecuencyInMHz;
-    float bandwidhtInKHz;
+    float frequencyInMHz;
+    float bandwidthInKHz;
 
     uint8_t spreadingFactor;
     uint8_t codingRate;
     uint8_t syncWord;
     uint8_t outputPowerInDBm;
 
-    size_t preambleLenght;
+    size_t preambleLength;
     bool enableCRC;
 };
 
-template<size_t PackageLenght>
 class LoRaCommunicator {
 public:
     explicit LoRaCommunicator(SX1262& radio);
 
     void initializeRadio(const LoRaParameters& parameters);
 
-    void sendPackage(const std::array<uint8_t, PackageLenght>& package);
+    void sendPackage(const std::vector<uint8_t>& package);
 
     [[nodiscard]]
-    std::array<uint8_t, PackageLenght> receivePackage(size_t timeoutInms);
+    std::vector<uint8_t> receivePackage(const size_t timeoutInms);
 
 private:
+    static constexpr size_t BUFFER_SIZE{ 32 };
+
     SX1262& radio_m;
 
-    std::array<uint8_t, PackageLenght> receivedDataBuffer;
+    std::vector<uint8_t> receivedDataBuffer_m;
+
+    template<class ExceptionType>
+    void throwIfError(int16_t errorCode, const std::string& message);
+
+    int16_t attemptReceive(const size_t timeoutInms);
 };
 
-template<size_t PackageLenght>
-inline LoRaCommunicator<PackageLenght>::LoRaCommunicator(SX1262 &radio)
-    : radio_m{ radio }
-{
-}
 
-template<size_t PackageLenght>
-inline void LoRaCommunicator<PackageLenght>::initializeRadio(const LoRaParameters& parameters) {
-    const int16_t radioStatus{ 
-        radio_m.begin(
-            parameters.frecuencyInMHz,
-            parameters.bandwidhtInKHz, 
-            parameters.spreadingFactor,
-            parameters.codingRate, 
-            parameters.syncWord,
-            parameters.outputPowerInDBm,
-            parameters.preambleLenght
-        )
-    };
-
-    if (radioStatus != RADIOLIB_ERR_NONE) {
-        throw std::invalid_argument{ "Unable to initialize Radio with the given parameters" };
+template <class ExceptionType>
+inline void LoRaCommunicator::throwIfError(int16_t errorCode, const std::string &message) {
+    if (errorCode != RADIOLIB_ERR_NONE) {
+        throw ExceptionType{ message };
     }
-}
-
-template <size_t PackageLenght>
-inline void LoRaCommunicator<PackageLenght>::sendPackage(const std::array<uint8_t, PackageLenght> &package) {
-    const int16_t transmisionStatus{ radio_m.transmit(package.data(), package.size()) };
-
-    if (transmisionStatus != RADIOLIB_ERR_NONE) {
-        throw std::runtime_error{ "Unable to send the package" };
-    }
-}
-
-template <size_t PackageLenght>
-inline std::array<uint8_t, PackageLenght> LoRaCommunicator<PackageLenght>::receivePackage(size_t timeoutInms) {
-    const uint32_t startTime{ millis() };
-    uint32_t timeElapsed{ 0 };
-
-    int16_t receiveStatus{ radio_m.receive(receivedDataBuffer.data(), receivedDataBuffer.size()) };
-
-    while ((timeElapsed < timeoutInms) && (receiveStatus == RADIOLIB_ERR_RX_TIMEOUT)) {
-        receiveStatus = radio_m.receive(receivedDataBuffer.data(), receivedDataBuffer.size());
-
-        timeElapsed = (millis() - startTime);
-        delay(1);
-    }
-
-    if (receiveStatus != RADIOLIB_ERR_NONE) {
-        throw std::runtime_error{ "Unable to receive the package" };
-    }
-    
-    return receivedDataBuffer;
 }
 
 #endif // !LORA_COMMUNICATOR_HEADER
---
+++ src/LoRaCommunicator.cpp
@@ -0,0 +1,57 @@
+#include "LoRaCommunicator.hpp"
+
+LoRaCommunicator::LoRaCommunicator(SX1262 &radio)
+    : radio_m{ radio }
+    , receivedDataBuffer_m(BUFFER_SIZE)
+{
+}
+
+void LoRaCommunicator::initializeRadio(const LoRaParameters& parameters) {
+    const int16_t radioStatus{ 
+        radio_m.begin(
+            parameters.frequencyInMHz,
+            parameters.bandwidthInKHz, 
+            parameters.spreadingFactor,
+            parameters.codingRate, 
+            parameters.syncWord,
+            parameters.outputPowerInDBm,
+            parameters.preambleLength
+        )
+    };
+
+    throwIfError<std::invalid_argument>(radioStatus, "Unable to initialize Radio with the given arguments");
+}
+
+void LoRaCommunicator::sendPackage(const std::vector<uint8_t> &package) {
+    const int16_t transmisionStatus{ radio_m.transmit(package.data(), package.size()) };
+
+    throwIfError<std::runtime_error>(transmisionStatus, "Unable to send the package");
+}
+
+std::vector<uint8_t> LoRaCommunicator::receivePackage(const size_t timeoutInms) {
+    receivedDataBuffer_m.resize(BUFFER_SIZE);
+
+    const int16_t receiveStatus{ attemptReceive(timeoutInms) };
+
+    throwIfError<std::runtime_error>(receiveStatus, "Unable to receive the package");
+
+    receivedDataBuffer_m.resize(radio_m.getPacketLength(true));
+    
+    return receivedDataBuffer_m;
+}
+
+int16_t LoRaCommunicator::attemptReceive(const size_t timeoutInms) {
+    const uint32_t startTime{ millis() };
+    uint32_t timeElapsed{ 0 };
+
+    int16_t receiveStatus{ radio_m.receive(receivedDataBuffer_m.data(), receivedDataBuffer_m.size()) };
+
+    while ((timeElapsed < timeoutInms) && (receiveStatus == RADIOLIB_ERR_RX_TIMEOUT)) {
+        receiveStatus = radio_m.receive(receivedDataBuffer_m.data(), receivedDataBuffer_m.size());
+
+        timeElapsed = (millis() - startTime);
+        delay(1);
+    }
+
+    return receiveStatus;
+}
