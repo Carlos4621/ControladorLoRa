6788105f194ad10c9d428b049eaf72fe012edbee
Carlos Iván <carlosivanhb@hotmail.com>
2025-05-02 21:02:26 -0600

    Limpieza de código

--- lib/GUI/GUI.cpp
+++ lib/GUI/GUI.cpp
@@ -9,165 +9,159 @@ struct GUI::Style {
     static constexpr uint8_t DisplayMiddle_X_Position{ 64 };
     static constexpr uint8_t DisplayMiddle_Y_Position{ 32 };
 
+    struct Motor {
+        static constexpr uint8_t LeftLabel_X{ 5 };
+        static constexpr uint8_t LeftLabel_Y{ 29 };
+        static constexpr uint8_t LeftValue_X{ 15 };
+        static constexpr uint8_t LeftValue_Y{ 29 };
 
-    static constexpr uint8_t LeftLabel_X_Position{ 17 };
-    static constexpr uint8_t LeftLabel_Y_Position{ 34 };
+        static constexpr uint8_t RightLabel_X{ 93 };
+        static constexpr uint8_t RightLabel_Y{ 29 };
+        static constexpr uint8_t RightValue_X{ 103 };
+        static constexpr uint8_t RightValue_Y{ 29 };
+    };
 
-    static constexpr uint8_t LeftMotorValue_X_Position{ 40 };
-    static constexpr uint8_t LeftMotorValue_Y_Position{ 34 };
+    struct Camera {
+        static constexpr uint8_t XLabel_X{ 44 };
+        static constexpr uint8_t XLabel_Y{ 25 };
+        static constexpr uint8_t XValue_X{ 60 };
+        static constexpr uint8_t XValue_Y{ 25 };
 
+        static constexpr uint8_t YLabel_X{ 44 };
+        static constexpr uint8_t YLabel_Y{ 34 };
+        static constexpr uint8_t YValue_X{ 60 };
+        static constexpr uint8_t YValue_Y{ 35 };
+    };
 
-    static constexpr uint8_t RightLabel_X_Position{ 66 };
-    static constexpr uint8_t RightLabel_Y_Position{ 34 };
+    struct Mode {
+        static constexpr uint8_t Label_X{ 1 };
+        static constexpr uint8_t Label_Y{ 51 };
+        static constexpr uint8_t Value_X{ 31 };
+        static constexpr uint8_t Value_Y{ 51 };
+    };
 
-    static constexpr uint8_t RightMotorValue_X_Position{ 94 };
-    static constexpr uint8_t RightMotorValue_Y_Position{ 34 };
+    struct Fixed {
+        static constexpr uint8_t Label_X{ 80 };
+        static constexpr uint8_t Label_Y{ 51 };
+        static constexpr uint8_t Value_X{ 109 };
+        static constexpr uint8_t Value_Y{ 51 };
+    };
 
+    struct Signal {
+        static constexpr uint8_t RSSILabel_X{ 1 };
+        static constexpr uint8_t RSSILabel_Y{ 1 };
+        static constexpr uint8_t RSSIValue_X{ 26 };
+        static constexpr uint8_t RSSIValue_Y{ 1 };
 
-    static constexpr uint8_t ModeLabel_X_Position{ 0 };
-    static constexpr uint8_t ModeLabel_Y_Position{ 52 };
-
-    static constexpr uint8_t ModeSelected_X_Position{ 31 };
-    static constexpr uint8_t ModeSelected_Y_Position{ 52 };
-
-
-    static constexpr uint8_t FixedLabel_X_Position{ 77 };
-    static constexpr uint8_t FixedLabel_Y_Position{ 52 };
-
-    static constexpr uint8_t FixedValue_X_Position{ 107 };
-    static constexpr uint8_t FixedValue_Y_Position{ 52 };
-
-
-    static constexpr uint8_t RSSILabel_X_Position{ 0 };
-    static constexpr uint8_t RSSILabel_Y_Position{ 0 };
-
-    static constexpr uint8_t RSSIValue_X_Position{ 26 };
-    static constexpr uint8_t RSSIValue_Y_Position{ 0 };
-
-
-    static constexpr uint8_t SNRLabel_X_Position{ 0 };
-    static constexpr uint8_t SNRLabel_Y_Position{ 12 };
-
-    static constexpr uint8_t SNRValue_X_Position{ 24 };
-    static constexpr uint8_t SNRValue_Y_Position{ 12 };
+        static constexpr uint8_t SNRLabel_X{ 1 };
+        static constexpr uint8_t SNRLabel_Y{ 13 };
+        static constexpr uint8_t SNRValue_X{ 25 };
+        static constexpr uint8_t SNRValue_Y{ 13 };
+    };
 };
 
 void GUI::displayWaitingConnection() {
-    configureConnectingWaitingFont();
-
+    configureFont(16, TEXT_ALIGN_CENTER_BOTH);
     display_m.clear();
-
     display_m.drawString(Style::DisplayMiddle_X_Position, Style::DisplayMiddle_Y_Position, "Conectando...");
-
     display_m.display();
 }
 
 void GUI::displayConnectionSuccesfull() {
-    configureConnectionSuccesfullFont();
-
+    configureFont(24, TEXT_ALIGN_CENTER_BOTH);
     display_m.clear();
-
     display_m.drawString(Style::DisplayMiddle_X_Position, Style::DisplayMiddle_Y_Position, "Conectado");
-
     display_m.display();
 }
 
 void GUI::displayError(std::string_view errorString) {
-    configureErrorFont();
-
+    configureFont(10, TEXT_ALIGN_LEFT);
     display_m.clear();
 
     std::string textToShow{ "Error:\n" };
-
     textToShow.append(errorString);
     textToShow.append("\n\nPlease restart");
 
     display_m.write(textToShow.c_str());
-
     display_m.display();
 }
 
 void GUI::showGUI(const ControllerData& controllerData, float RSSI, float SNR) {
-    configureGUIFont();
-
+    configureFont(10, TEXT_ALIGN_LEFT);
     display_m.clear();
 
     if (controllerData.selectedMode == Modes::Modes_FIXED_SPEED) {
         auto [rightMotorValue, leftMotorValue] = MotorController::getFixedSpeedValuesConverted(controllerData);
-
-        displayLeftMotorData(leftMotorValue);
-        displayRightMotorData(rightMotorValue);
+        
+        displayLabelAndValue("L:", leftMotorValue, 
+            Style::Motor::LeftLabel_X, Style::Motor::LeftLabel_Y, 
+            Style::Motor::LeftValue_X, Style::Motor::LeftValue_Y);
+            
+        displayLabelAndValue("R:", rightMotorValue, 
+            Style::Motor::RightLabel_X, Style::Motor::RightLabel_Y, 
+            Style::Motor::RightValue_X, Style::Motor::RightValue_Y);
     }
     else {
-        displayLeftMotorData(static_cast<int8_t>(controllerData.leftJoystick.axisY));
-        displayRightMotorData(static_cast<int8_t>(controllerData.rightJoystick.axisY));
+        displayLabelAndValue("L:", static_cast<int8_t>(controllerData.leftJoystick.axisY), 
+            Style::Motor::LeftLabel_X, Style::Motor::LeftLabel_Y, 
+            Style::Motor::LeftValue_X, Style::Motor::LeftValue_Y);
+            
+        displayLabelAndValue("R:", static_cast<int8_t>(controllerData.rightJoystick.axisY), 
+            Style::Motor::RightLabel_X, Style::Motor::RightLabel_Y, 
+            Style::Motor::RightValue_X, Style::Motor::RightValue_Y);
     }
 
-    displaySelectedMode(controllerData.selectedMode);
-    displayFixedValue(static_cast<int8_t>(controllerData.fixedSpeed));
+    displayLabelAndValue("Cx:", static_cast<int8_t>(controllerData.cameraJoystick.axisX), 
+        Style::Camera::XLabel_X, Style::Camera::XLabel_Y, 
+        Style::Camera::XValue_X, Style::Camera::XValue_Y);
+        
+    displayLabelAndValue("Cy:", static_cast<int8_t>(controllerData.cameraJoystick.axisY), 
+        Style::Camera::YLabel_X, Style::Camera::YLabel_Y, 
+        Style::Camera::YValue_X, Style::Camera::YValue_Y);
 
-    displayRSSIData(RSSI);
-    displaySNRData(SNR);
+    displayLabelAndValue("Mode:", controllerData.selectedMode,
+        Style::Mode::Label_X, Style::Mode::Label_Y, 
+        Style::Mode::Value_X, Style::Mode::Value_Y);
+        
+    displayLabelAndValue("Fixed:", static_cast<int8_t>(controllerData.fixedSpeed),
+        Style::Fixed::Label_X, Style::Fixed::Label_Y, 
+        Style::Fixed::Value_X, Style::Fixed::Value_Y);
+
+    displayLabelAndValue("RSSI:", RSSI,
+        Style::Signal::RSSILabel_X, Style::Signal::RSSILabel_Y, 
+        Style::Signal::RSSIValue_X, Style::Signal::RSSIValue_Y, " dBm");
+        
+    displayLabelAndValue("SNR:", SNR,
+        Style::Signal::SNRLabel_X, Style::Signal::SNRLabel_Y, 
+        Style::Signal::SNRValue_X, Style::Signal::SNRValue_Y, " dB");
     
     display_m.display();
 }
 
-void GUI::configureConnectingWaitingFont() {
-    display_m.setFont(ArialMT_Plain_16);
-    display_m.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+void GUI::configureFont(uint8_t size, OLEDDISPLAY_TEXT_ALIGNMENT alignment) {
+    switch (size) {
+        case 24:
+            display_m.setFont(ArialMT_Plain_24);
+            break;
+        case 16:
+            display_m.setFont(ArialMT_Plain_16);
+            break;
+        case 10:
+        default:
+            display_m.setFont(ArialMT_Plain_10);
+            break;
+    }
+    display_m.setTextAlignment(alignment);
 }
 
-void GUI::configureConnectionSuccesfullFont() {
-    display_m.setFont(ArialMT_Plain_24);
-    display_m.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-}
+template<typename T>
+void GUI::displayLabelAndValue(std::string_view label, T value, uint8_t labelX, uint8_t labelY, uint8_t valueX,
+    uint8_t valueY, std::string_view suffix) {
 
-void GUI::configureErrorFont() {
-    display_m.setFont(ArialMT_Plain_10);
-    display_m.setTextAlignment(TEXT_ALIGN_LEFT);
-}
-
-void GUI::configureGUIFont() {
-    display_m.setFont(ArialMT_Plain_10);
-    display_m.setTextAlignment(TEXT_ALIGN_LEFT);
-}
-
-void GUI::displayRightMotorData(int8_t value) {
-    display_m.drawString(Style::RightLabel_X_Position, Style::RightLabel_Y_Position, "Right:");
-    display_m.drawString(Style::RightMotorValue_X_Position, Style::RightLabel_Y_Position, std::to_string(value).c_str());
-}
-
-void GUI::displayLeftMotorData(int8_t value) {
-    display_m.drawString(Style::LeftLabel_X_Position, Style::LeftLabel_Y_Position, "Left:");
-    display_m.drawString(Style::LeftMotorValue_X_Position, Style::LeftMotorValue_Y_Position, std::to_string(value).c_str());
-}
-
-void GUI::displaySelectedMode(uint8_t value) {
-    display_m.drawString(Style::ModeLabel_X_Position, Style::ModeLabel_Y_Position, "Mode:");
-    display_m.drawString(Style::ModeSelected_X_Position, Style::ModeLabel_Y_Position, std::to_string(value).c_str());
-}
-
-void GUI::displayFixedValue(int8_t value) {
-    display_m.drawString(Style::FixedLabel_X_Position, Style::FixedLabel_Y_Position, "Fixed:");
-    display_m.drawString(Style::FixedValue_X_Position, Style::FixedValue_Y_Position, std::to_string(value).c_str());
-}
-
-void GUI::displayRSSIData(float value) {
-
-    std::string textToShow{ std::to_string(value) };
-
-    textToShow += " dBm";
-
-    display_m.drawString(Style::RSSILabel_X_Position, Style::RSSILabel_Y_Position, "RSSI:");
-    display_m.drawString(Style::RSSIValue_X_Position, Style::RSSIValue_Y_Position, textToShow.c_str());
-}
-
-void GUI::displaySNRData(float value) {
-
-    std::string textToShow{ std::to_string(value) };
-
-    textToShow += " dB";
-
-    display_m.drawString(Style::SNRLabel_X_Position, Style::SNRLabel_Y_Position, "SNR:");
-    display_m.drawString(Style::SNRValue_X_Position, Style::SNRValue_Y_Position, textToShow.c_str());
+    display_m.drawString(labelX, labelY, label.data());
+    
+    std::string valueStr{ std::to_string(value) };
+    valueStr += suffix;
+    
+    display_m.drawString(valueX, valueY, valueStr.c_str());
 }
--- lib/GUI/GUI.hpp
+++ lib/GUI/GUI.hpp
@@ -4,8 +4,9 @@
 #include <pb.h>
 #include "Buttons.pb.h"
 #include "Modes.pb.h"
-#include <heltec_unofficial.h>
+#include <heltec_unofficial.h>  
 #include <string>
+#include <string_view>
 #include "MotorController.hpp"
 
 /// @brief Clase que permite mostrar datos en patalla
@@ -34,20 +35,15 @@ public:
 
 private:
     struct Style;
+    struct DisplayElement;
 
     SSD1306Wire& display_m;
 
-    void configureConnectingWaitingFont();
-    void configureConnectionSuccesfullFont();
-    void configureErrorFont();
-    void configureGUIFont();
-
-    void displayRightMotorData(int8_t value);
-    void displayLeftMotorData(int8_t value);
-    void displaySelectedMode(uint8_t value);
-    void displayFixedValue(int8_t value);
-    void displayRSSIData(float value);
-    void displaySNRData(float value);
+    void configureFont(uint8_t size, OLEDDISPLAY_TEXT_ALIGNMENT alignment);
+    
+    template<typename T>
+    void displayLabelAndValue(std::string_view label, T value, uint8_t labelX, uint8_t labelY, uint8_t valueX,
+        uint8_t valueY, std::string_view suffix = "");
 };
 
 #endif // !GUI_HEADER
